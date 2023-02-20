c094b765b716348fa35b065538f36f3ae4d92fa1
rodVieira021 <rodvieira021@gmail.com>
2023-02-20 19:54:50 +0200

    checkout3

--- src/Components/Pages/Checkout.jsx
+++ src/Components/Pages/Checkout.jsx
@@ -4,11 +4,22 @@ import "./Checkout.css";
 import { Link } from "react-router-dom";
 import Footer from "../Partials/Footer";
 import { cart } from "../../Redux/productSlice";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import payment from "../.././Images/cart/payment-img.jpg";
+import { useState } from "react";
 
 const Checkout = () => {
   const checkoutCart = useSelector(cart);
+  const [hreturning, setHreturning] = useState({ display: "none" });
+  const [hcoupon, setHcoupon] = useState({ display: "none" });
+
+  const changeReturning = () => {
+    setHreturning({ display: "block" });
+  };
+  const changeCoupon = () => {
+    setHcoupon({ display: "block" });
+  };
+
   return (
     <div>
       <div className="header">
@@ -18,31 +29,36 @@ const Checkout = () => {
         <div className="left">
           <h4 className="left-header">
             {" "}
-            RETURNING CUSTOMER? <Link>CLICK HERE TO LOGIN</Link>
+            RETURNING CUSTOMER?{" "}
+            <Link onClick={() => changeReturning()}>CLICK HERE TO LOGIN</Link>
           </h4>
           <div className="form-login">
-            <p>
-              If you have shopped with us before, please enter your details in
-              the boxes below. If you are a new customer, please proceed to the
-              Billing & Shipping section.
-            </p>
-            <form className="form-left grid col-2">
-              <form className="inputs-1 flex">
-                <label htmlFor="email-input">Email address</label>
-                <input type="text" className="email-input" />
-                <div className="check-div flex">
-                  <input type="checkbox" className="check-login" />
-                  <label htmlFor="check-login">Remember me!</label>
+            <div style={hreturning} className="form-text">
+              <p>
+                If you have shopped with us before, please enter your details in
+                the boxes below. If you are a new customer, please proceed to
+                the Billing & Shipping section.
+              </p>
+
+              <form className="form-left grid col-2">
+                <form className="inputs-1 flex">
+                  <label htmlFor="email-input">Email address</label>
+                  <input type="text" className="email-input" />
+                  <div className="check-div flex">
+                    <input type="checkbox" className="check-login" />
+                    <label htmlFor="check-login">Remember me!</label>
+                  </div>
+                </form>
+                <div className="inputs-2 flex">
+                  <label htmlFor="password-input">Password *</label>
+                  <input type="text" className="password-input" />
+                  <Link href="pass-forgot">Forgot your password?</Link>
                 </div>
+                <button className="login-submit">SUBMIT</button>
               </form>
-              <div className="inputs-2 flex">
-                <label htmlFor="password-input">Password *</label>
-                <input type="text" className="password-input" />
-                <Link href="pass-forgot">Forgot your password?</Link>
-              </div>
-              <button className="login-submit">SUBMIT</button>
-            </form>
+            </div>
           </div>
+
           <div className="billing-container">
             <h4>BILLING DETAILS</h4>
             <form className="form-billing grid col-2">
@@ -86,9 +102,12 @@ const Checkout = () => {
 
         <div className="right">
           <h4 className="right-header">
-            HAVE A COUPON?<Link>CLICK HERE TO ENTER YOUR CODE</Link>
+            HAVE A COUPON?
+            <Link onClick={() => changeCoupon()}>
+              CLICK HERE TO ENTER YOUR CODE
+            </Link>
           </h4>
-          <div className="form-container-cupoun">
+          <div style={hcoupon} className="form-container-cupoun">
             <p>Enter your coupon code if you have one</p>
             <form className="form-coupon flex">
               <label htmlFor="input-coupon">* Coupon</label>
